6c1f9d68b27e35e54d8c2703116fdbb514b8acea
AllanT102 <allan.zf.tan@gmail.com>
2022-09-08 12:57:02 -0700

    Added responsiveness

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -4,7 +4,7 @@ const Scoreboard = (props) => {
 
     return (
         <div style={{display: "flex", flexDirection:"column", width: "100%", justifyContent: "center", gap: "5%", alignItems:"center"}}>
-            <p style={{fontSize: "1.7em"}}>Click on pokemon that you haven't clicked on before!</p>
+            <p style={{fontSize: "1.7em", textAlign:"center", padding: "0 10px"}}>Click on pokemon that you haven't clicked on before!</p>
             <div style={{display: "flex", width: "100%", justifyContent: "center", gap: "5%"}}>
                 <p style={{fontSize: "1.7em"}}>Score: {props.score}</p>
                 <p style={{fontSize: "1.7em"}}>Highscore: {props.hs}</p>
